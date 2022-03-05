55af6f85ea7106add41202dfefa164cb40bb8b7d
Samiul <sicse97@gmail.com>
2022-03-05 17:41:06 +0530

    Initial commit

---
+++ sa.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num1, num2,sum = 0;
+
+    printf("Enter the number :\n");
+    scanf("%d",&num1);
+
+    printf("Enter the number :\n");
+    scanf("%d",&num2);
+    
+    sum = num1 + num2;
+
+    printf("Sum is = %d",sum);
+
+ getch();
+
+}
